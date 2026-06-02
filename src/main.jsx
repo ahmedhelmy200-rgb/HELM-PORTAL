28df1b1791425cf149ebae839a85dbd8a681137f
ahmed helmy <ahmedhelmy200@gmail.com>
2026-06-02 06:49:02 +0400

    Load polish stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 import '@/readability.css'
+import '@/polish.css'
 import { installGlobalImageRecovery } from '@/lib/imageRecovery'
 
 installGlobalImageRecovery()
